aa8002df1ebc5749bea7b4779a0a7f93c8787170
Jordan Lerat <jlerat@rrc.ca>
2026-06-17 10:32:47 -0500

    Added Active Admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+AdminUser.find_or_create_by!(email: 'admin@example.com') do |admin|
+  admin.password = 'password'
+  admin.password_confirmation = 'password'
+end
