e3e03d86794071614b23797f95285def11be2c10
Jordan Lerat <jlerat@rrc.ca>
2026-06-17 11:13:11 -0500

    Completed CRM setup with ActiveAdmin and validations

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -1,18 +1,24 @@
 ActiveAdmin.register Customer do
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :full_name, :phone_number, :email_address, :notes
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:full_name, :phone_number, :email_address, :notes]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  # Added image to the list of permitted parameters
+  permit_params :full_name, :phone_number, :email_address, :notes, :image
+
+  # Added filters for the new attributes
+  filter :full_name
+  filter :phone_number
+  filter :email_address
+  filter :notes
+
+  # Added image to the index page
+  form do |f|
+    f.inputs do
+      f.input :full_name
+      f.input :phone_number
+      f.input :email_address
+      f.input :notes
+      f.input :image, as: :file
+    end
+
+    f.actions
+  end
 end
