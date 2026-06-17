07eb564b14cbe9e658de1123210377cfde580f7c
Jordan Lerat <jlerat@rrc.ca>
2026-06-17 13:39:45 -0500

    Added documentation

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,16 +1,16 @@
 class CustomersController < ApplicationController
 
-  # GET /customers
+  # GET /customers loads every customer from the database
   def index
     @customers = Customer.all
   end
 
-  # GET /customers/alphabetized
+  # GET /customers/alphabetized sorts customers alphabetically
   def alphabetized
     @customers = Customer.order(:full_name)
   end
 
-  # GET /customers/missing_email
+  # GET /customers/missing_email finds customers without an email address
   def missing_email
     @customers = Customer.where(email_address: [nil, ""])
   end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,10 +1,18 @@
+# Generated Customer model
 class Customer < ApplicationRecord
+
+  # Customers can have an image attached
   has_one_attached :image
 
+  # Customer validations
+  # Every customer should have a name
   validates :full_name, presence: true
+  # Every customer should have a phone number
   validates :phone_number, presence: true
+  # Email can be blank, if one is entered it must be unique
   validates :email_address, uniqueness: true, allow_blank: true
 
+  #
   def self.ransackable_attributes(auth_object = nil)
     ["created_at","email_address", "full_name", "id", "notes", "phone_number", "updated_at"]
   end
